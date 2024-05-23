4da9d383845fb9532b24bdcfaa485132292cb1cb
Letícia S. Lenzoni <125830543+leticiascofield@users.noreply.github.com>
2024-05-23 19:13:16 -0300

    Update Livro.cpp

--- src/Livro.cpp
+++ src/Livro.cpp
@@ -5,17 +5,17 @@ Livro::Livro() : nome(""), quantidade(0) {}
 Livro::Livro(std::string nome, int quantidade) : nome(nome), quantidade(quantidade) {}
 
 std::string Livro::getNome() const {
-    return nome;
+    return this->nome;
 }
 
 int Livro::getQuantidade() const {
-    return quantidade;
+    return this->quantidade;
 }
 
 bool Livro::operator==(const Livro& l) const {
-    return nome == l.nome;
+    return this->nome == l.nome;
 }
 
 bool Livro::operator<(const Livro& l) const {
-    return nome < l.nome;
+    return this->nome < l.nome;
 }
